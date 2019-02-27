377633fe72f518cab2ab58fb9609ecee15155bb5
Riebbels Willem <Willem.Riebbels@digipolis.gent>
2019-02-27 13:19:04 +0100

    Update New-PasswordStatePassword.ps1
    Replace foreach % with foreach-object, to consume less memory.
    fixed logic in test if number is lower than 10

--- functions/New-PasswordStatePassword.ps1
+++ functions/New-PasswordStatePassword.ps1
@@ -66,9 +66,9 @@ function New-PasswordStatePassword {
         }
         # Check if hashtable has valid values
         IF($genericfields) {
-            $genericfields.keys | %{
-                if(!($($_) -match "GenericField\d" -and !($($_ -replace "[^0-9]" , '') -le 10))){
-                    throw "genericfield array is not between boundaries or has invalid key names genericfield[1-10]"
+            $genericfields.keys | ForEach-Object -process {
+                if(!($($_) -match "GenericField\d" -and $($_ -replace "[^0-9]" , '') -le 10)){
+                    throw "GenericField array is not between boundaries or has invalid key names GenericField[1-10]"
                 }
             }
         }
