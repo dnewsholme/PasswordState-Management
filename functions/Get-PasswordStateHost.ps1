5773719feb9cfac3136142a612620daf70ad519f
colombeen <jarno@colombeen.be>
2019-06-20 10:26:16 +0200

    Minor - *-PasswordStateHost (#55)
    * Minor - *-PasswordStateHost
    
    New function to add/remove/get hosts in the system

---
+++ functions/Get-PasswordStateHost.ps1
@@ -0,0 +1,83 @@
+﻿<#
+    .SYNOPSIS
+    Finds a password state host and returns the object. If multiple matches it will return multiple entries.
+
+    .DESCRIPTION
+    Finds a password state host and returns the object. If multiple matches it will return multiple entries.
+
+    .EXAMPLE
+    PS C:\> Get-PasswordStateHost
+    Returns all hosts you have access to.
+    .EXAMPLE
+    PS C:\> Get-PasswordStateHost 'testhost'
+    Returns the test host object.
+    .EXAMPLE
+    PS C:\> Get-PasswordStateHost -OperatingSystem 'Windows Server 2012'
+    Returns the hosts that are using the Windows Server 2012 operating system.
+    .EXAMPLE
+    PS C:\> Get-PasswordStateHost -DatabaseServerType 'SQL Server,Oracle'
+    Returns the hosts that are of the database server type SQL Servers and Oracle
+
+    .PARAMETER HostName
+    An optional parameter to filter the search on hostname.
+    .PARAMETER HostType
+    An optional parameter to filter the search on type of host.
+    .PARAMETER OperatingSystem
+    An optional parameter to filter the search on operating system.
+    .PARAMETER DatabaseServerType
+    An optional parameter to filter the search on database server type.
+    .PARAMETER SiteID
+    An optional parameter to filter the search on the site ID.
+    .PARAMETER SiteLocation
+    An optional parameter to filter the search on the site location.
+
+    .PARAMETER PreventAuditing
+    An optional parameter to prevent logging this API call in the audit log (Can be overruled in PasswordState preferences).
+
+    .OUTPUTS
+    Returns the Object from the API as a powershell object.
+
+    .NOTES
+    2019 - Jarno Colombeen
+#>
+Function Get-PasswordStateHost
+{
+  [CmdletBinding()]
+  Param
+  (
+    [Parameter(ValueFromPipeline, ValueFromPipelineByPropertyName, Position = 0)][string]$HostName,
+    [Parameter(ValueFromPipelineByPropertyName, Position = 1)][string]$HostType,
+    [Parameter(ValueFromPipelineByPropertyName, Position = 2)][string]$OperatingSystem,
+    [Parameter(ValueFromPipelineByPropertyName, Position = 3)][string]$DatabaseServerType,
+    [Parameter(ValueFromPipelineByPropertyName, Position = 4)][int32]$SiteID,
+    [Parameter(ValueFromPipelineByPropertyName, Position = 5)][string]$SiteLocation,
+    [Parameter(ValueFromPipelineByPropertyName, Position = 6)][switch]$PreventAuditing
+  )
+
+  Process {
+    $uri = "/api/hosts/"
+
+    If ($PSBoundParameters.Count -gt 0)
+    {
+      $BuildURL = '?'
+      If ($HostName) {                  $BuildURL += "HostName=$([System.Web.HttpUtility]::UrlEncode($HostName))&" }
+      If ($HostType) {                  $BuildURL += "HostType=$([System.Web.HttpUtility]::UrlEncode($HostType))&" }
+      If ($OperatingSystem) {           $BuildURL += "OperatingSystem=$([System.Web.HttpUtility]::UrlEncode($OperatingSystem))&" }
+      If ($DatabaseServerType) {        $BuildURL += "DatabaseServerType=$([System.Web.HttpUtility]::UrlEncode($DatabaseServerType))&" }
+      If ($SiteID) {                    $BuildURL += "SiteID=$([System.Web.HttpUtility]::UrlEncode($SiteID))&" }
+      If ($SiteLocation) {              $BuildURL += "SiteLocation=$([System.Web.HttpUtility]::UrlEncode($SiteLocation))&" }
+      If ($PreventAuditing.IsPresent) { $BuildURL += "PreventAuditing=true&" }
+
+      $uri += ($BuildURL -Replace ".$")
+    }
+
+    Try
+    {
+      Get-PasswordStateResource -URI $uri -Method GET
+    }
+    Catch
+    {
+      Throw $_.Exception
+    }
+  }
+}
---
+++ functions/New-PasswordStateHost.ps1
@@ -0,0 +1,141 @@
+﻿<#
+    .SYNOPSIS
+    Creates a New Password State host.
+    .DESCRIPTION
+    Creates a New Password State host.
+
+    .EXAMPLE
+    PS C:\> New-PasswordStateHost -HostName 'TestServer.domain.local' -HostType Windows -OperatingSystem 'Windows 10' -RemoteConnectionType RDP
+    Creates a new host with the hostname 'TestServer.domain.local'
+
+    .PARAMETER HostName
+    The name of the Host. FQDN host names are preferred if possible.
+    .PARAMETER HostType
+    A Host Type which describes the Host i.e. Windows, Linux, Switch, Router, etc. A list of Host Types can be found on the screen Administration -> Passwordstate Administration -> Host Types and Operating Systems.
+    .PARAMETER OperatingSystem
+    The type of Operating System the Host is using. A list of Operting Systems can be found on the screen Administration -> Passwordstate Administration -> Host Types and Operating Systems.
+    .PARAMETER DatabaseServerType
+    Optional parameter that specifies the type of Database Server if applicable - MariaDB, MySQL, Oracle, PostgreSQL or SQL Server.
+    .PARAMETER SQLInstanceName
+    Optional parameter that provides either the Microsoft SQL Server Instance Name, Oracle Service Name or PostgreSQL Database Name.
+    .PARAMETER DatabasePortNumber
+    Optional parameter that indicates the Port Number the database server is accessible on. Leaving the Port Number blank is most cases should work, but you can specify non-standard port numbers if required
+    .PARAMETER RemoteConnectionType
+    The type of Remote Connection Protocol for the server i.e. RDP, SSH, Teamviewer, Telnet or VNC.
+    .PARAMETER RemoteConnectionPortNumber
+    The Port Number used for the Remote Connection type. Default Port Numbers are RDP: 3389, SSH: 22, Teamviewer: 0, Telnet: 23 and VNC: 5901.
+    .PARAMETER RemoteConnectionParameters
+    Optional parameter. If the required Remote Session Client requires additional parameters to be specified to connect to the remote Host, they can be specified using this field.
+    .PARAMETER Tag
+    Optional descriptive Tag for the Host record.
+    .PARAMETER Title
+    Optional title field. If this has a value, it will be displayed in the Hosts Navigation Tree instead.
+    .PARAMETER SiteID
+    Optional parameter. The SiteID the Host record belongs to (SiteID of 0 for site of type Internal).
+    .PARAMETER InternalIP
+    Optional parameter to specify the Internal IP Address of the Host.
+    .PARAMETER ExternalIP
+    Optional parameter. The Externally facing IP Address of the Host i.e. if an Azure, Amazon, or an internally hosted server which has been exposed external to your standard network.
+    .PARAMETER MACAddress
+    Optional parameter to define the network MAC Address for the Host.
+    .PARAMETER SessionRecording
+    Optional switch. Indicates whether all sessions will be recorded for this host record.
+    .PARAMETER VirtualMachine
+    Optional switch that specifies if this is a Virtual Machine or not.
+    .PARAMETER VirtualMachineType
+    Optional parameter that, if this is a Virtual Machine, defines if it is of type Amazon, Azure, HyperV, Virtualbox, VMware or Xen.
+    .PARAMETER Notes
+    Optional parameter with any additional Notes for the Host.
+
+    .PARAMETER PreventAuditing
+    An optional parameter to prevent logging this API call in the audit log (Can be overruled in PasswordState preferences).
+
+    .OUTPUTS
+    The created host is returned as an object.
+
+    .NOTES
+    2019 - Jarno Colombeen
+#>
+Function New-PasswordStateHost
+{
+  [CmdletBinding(SupportsShouldProcess = $true,DefaultParameterSetName = 'Default')]
+  Param (
+    [Parameter(ValueFromPipelineByPropertyName, Position = 0, Mandatory = $true)][string]$HostName,
+    [Parameter(ValueFromPipelineByPropertyName, Position = 1, Mandatory = $true, HelpMessage = 'A list of Host Types can be found on the screen Administration -> Passwordstate Administration -> Host Types and Operating Systems')][ValidateLength(0,50)][string]$HostType,
+    [Parameter(ValueFromPipelineByPropertyName, Position = 2, Mandatory = $true, HelpMessage = 'A list of Operting Systems can be found on the screen Administration -> Passwordstate Administration -> Host Types and Operating Systems')][ValidateLength(0,50)][string]$OperatingSystem,
+    [Parameter(ValueFromPipelineByPropertyName, Position = 3)][ValidateSet('MariaDB','MySQL','Oracle','PostgreSQL','SQL Server')][string]$DatabaseServerType,
+    [Parameter(ValueFromPipelineByPropertyName, Position = 4)][ValidateLength(0,100)][string]$SQLInstanceName = '',
+    [Parameter(ValueFromPipelineByPropertyName, Position = 5)][int32]$DatabasePortNumber,
+    [Parameter(ValueFromPipelineByPropertyName, Position = 6, Mandatory = $true)][ValidateSet('RDP','SSH','Teamviewer','Telnet','VNC')][string]$RemoteConnectionType,
+    [Parameter(ValueFromPipelineByPropertyName, Position = 7)][int32]$RemoteConnectionPortNumber,
+    [Parameter(ValueFromPipelineByPropertyName, Position = 8)][ValidateLength(0,500)][string]$RemoteConnectionParameters = '',
+    [Parameter(ValueFromPipelineByPropertyName, Position = 9)][ValidateLength(0,1000)][string]$Tag = '',
+    [Parameter(ValueFromPipelineByPropertyName, Position = 10)][ValidateLength(0,200)][string]$Title = '',
+    [Parameter(ValueFromPipelineByPropertyName, Position = 11)][int32]$SiteID = 0,
+    [Parameter(ValueFromPipelineByPropertyName, Position = 12)][ValidateLength(0,50)][string]$InternalIP = '',
+    [Parameter(ValueFromPipelineByPropertyName, Position = 13)][ValidateLength(0,50)][string]$ExternalIP = '',
+    [Parameter(ValueFromPipelineByPropertyName, Position = 14)][ValidateLength(0,50)][string]$MACAddress = '',
+    [Parameter(ValueFromPipelineByPropertyName, Position = 15)][switch]$SessionRecording,
+    [Parameter(ValueFromPipelineByPropertyName, Position = 16, ParameterSetName = 'VM')][switch]$VirtualMachine,
+    [Parameter(ValueFromPipelineByPropertyName, Position = 17, ParameterSetName = 'VM', Mandatory = $true)][ValidateSet('Amazon','Azure','HyperV','Virtualbox','VMware','Xen')][string]$VirtualMachineType,
+    [Parameter(ValueFromPipelineByPropertyName, Position = 18)][string]$Notes = '',
+    [Parameter(ValueFromPipelineByPropertyName, Position = 19)][switch]$PreventAuditing
+  )
+
+  Process
+  {
+    # Fix params
+    If (!($RemoteConnectionPortNumber))
+    {
+      Switch ($RemoteConnectionType)
+      {
+        'RDP'        { $RemoteConnectionPortNumber = 3389 }
+        'SSH'        { $RemoteConnectionPortNumber = 22 }
+        'Teamviewer' { $RemoteConnectionPortNumber = 0 }
+        'Telnet'     { $RemoteConnectionPortNumber = 23 }
+        'VNC'        { $RemoteConnectionPortNumber = 5901 }
+      }
+    }
+
+    $uri = ''
+    If ($PreventAuditing.IsPresent)
+    {
+      $uri = '?PreventAuditing=true'
+    }
+
+    # Create the post object
+    $Body = [PSCustomObject] @{
+      'HostName'                   = $HostName
+      'HostType'                   = $HostType
+      'OperatingSystem'            = $OperatingSystem
+      'DatabaseServerType'         = $DatabaseServerType
+      'SQLInstanceName'            = $SQLInstanceName
+      'DatabasePortNumber'         = $DatabasePortNumber
+      'RemoteConnectionType'       = $RemoteConnectionType
+      'RemoteConnectionPortNumber' = $RemoteConnectionPortNumber
+      'RemoteConnectionParameters' = $RemoteConnectionParameters
+      'Tag'                        = $Tag
+      'Title'                      = $Title
+      'SiteID'                     = $SiteID
+      'InternalIP'                 = $InternalIP
+      'ExternalIP'                 = $ExternalIP
+      'MACAddress'                 = $MACAddress
+      'SessionRecording'           = $SessionRecording.IsPresent
+      'VirtualMachine'             = $VirtualMachine.IsPresent
+      'VirtualMachineType'         = $VirtualMachineType
+      'Notes'                      = $Notes
+    }
+
+    If ($PSCmdlet.ShouldProcess("$($HostName)"))
+    {
+      Try
+      {
+        New-PasswordStateResource -uri "/api/hosts/$($uri)" -body "$($Body | ConvertTo-Json)" -method POST
+      }
+      Catch
+      {
+        Throw $_.Exception
+      }
+    }
+  }
+}
