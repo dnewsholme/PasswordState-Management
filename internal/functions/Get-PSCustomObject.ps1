334134bbd0c60ddd26a88284859c07518a0857d8
eizedev <github@eize.dev>
2020-04-29 12:53:23 +0200

    Added internal function 'Get-PSCustomObject'. This function is used to sort (Get-PSCustomObject -Sort) the body ($body) before executing the api query. Useful for debugging purposes and in combination with -Verbose

---
+++ internal/functions/Get-PSCustomObject.ps1
@@ -0,0 +1,37 @@
+function Get-PSCustomObject
+{
+    [CmdletBinding()]
+    Param(
+        [Parameter(ValueFromPipelineByPropertyName, ValueFromPipeline, Mandatory = $true)]
+        [PSCustomObject]$InputObject,
+        [Parameter(ValueFromPipelineByPropertyName, ValueFromPipeline, Mandatory = $false)]
+        [switch]$Sort,
+        [Parameter(ValueFromPipelineByPropertyName, ValueFromPipeline, Mandatory = $false)]
+        [switch]$Descending,
+        [Parameter(ValueFromPipelineByPropertyName, ValueFromPipeline, Mandatory = $false)]
+        [switch]$Unique,
+        [Parameter(ValueFromPipelineByPropertyName, ValueFromPipeline, Mandatory = $false)]
+        [switch]$CaseSensitive,
+        [Parameter(ValueFromPipelineByPropertyName, ValueFromPipeline, Mandatory = $false)]
+        [switch]$Stable
+    )
+    if ($Sort)
+    {
+        # Create a temporary new ordered hashtable
+        $ObjectSorted = [ordered]@{ }
+        # Get all properties from the given InputObject, sort it by name and add each property to the temporary created ordered hashtable
+        Get-Member -Type NoteProperty -InputObject $InputObject | Sort-Object -Unique:$Unique -Descending:$Descending -CaseSensitive:$CaseSensitive -Stable:$Stable -Property Name | ForEach-Object { $ObjectSorted[$_.Name] = $InputObject.$($_.Name) }
+        # Create a new PSCustomObject that will receive the sorted properties from the hashtable
+        $OutputObject = New-Object PSCustomObject
+        # Add each property the the newly created PSCustomObject
+        Add-Member -InputObject $OutputObject -NotePropertyMembers $ObjectSorted
+
+        # return the sorted object
+        return $OutputObject
+    }
+    else
+    {
+        Write-Warning "Nothing changed! Add '-Sort' if you want to sort the CustomObject"
+        return $InputObject
+    }
+}
