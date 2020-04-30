e3e32ed6e597ddc86a745cb882b8724aee2b1be1
Daryl Newsholme <dbizsley@googlemail.com>
2020-04-30 14:52:51 +0100

    Update 3.deploy.ps1

--- build/3.deploy.ps1
+++ build/3.deploy.ps1
@@ -7,7 +7,7 @@
     $apikey
 )
 $modulename = $($env:CI_PROJECT_NAME).tolower()
-
+import-module powershellget
 Remove-Item "$modulepath\$modulename" -Recurse -Force -erroraction silentlycontinue
 Copy-Item ".\$modulename" -Recurse -Destination $modulepath
 $commitmsg = (Get-BuildEnvironment).CommitMessage
