0676d96f5476b19e246da1d6bd452ae86c58e051
Tom Robijns <tom.robijns@realdolmen.com>
2020-05-05 00:52:14 +0200

    Add option for clear-text passwords

--- functions/Set-PasswordStateEnvironment.ps1
+++ functions/Set-PasswordStateEnvironment.ps1
@@ -1,15 +1,15 @@
 ﻿function Set-PasswordStateEnvironment {
     [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidUsingConvertToSecureStringWithPlainText', '', Justification = 'all passwords stored encrypted')]
     [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidUsingPlaintextForPassword', '', Justification = 'no password')]
-    [CmdletBinding(DefaultParameterSetName = "Two", SupportsShouldProcess = $true)]
+    [CmdletBinding(DefaultParameterSetName = "Two", SupportsShouldProcess, ConfirmImpact="High")]
     param (
         [Parameter(ValueFromPipelineByPropertyName,Mandatory = $true)][string]$Baseuri,
         [Parameter(ValueFromPipelineByPropertyName,ParameterSetName = 'One')][string]$Apikey,
         [Parameter(ValueFromPipelineByPropertyName,ParameterSetName = 'One')][string]$PasswordGeneratorAPIkey,
         [Parameter(ValueFromPipelineByPropertyName,ParameterSetName = 'Two')][switch]$WindowsAuthOnly,
         [Parameter(ValueFromPipelineByPropertyName,ParameterSetName = 'Three')][pscredential]$customcredentials,
-        [Parameter(ValueFromPipelineByPropertyName,Mandatory = $false)][string]$path = [Environment]::GetFolderPath('UserProfile')
-
+        [Parameter(ValueFromPipelineByPropertyName,Mandatory = $false)][string]$path = [Environment]::GetFolderPath('UserProfile'),
+        [Parameter()][bool]$SetPlainTextPasswords = $False
     )
 
     begin {
@@ -38,13 +38,11 @@
 
     process {
         # Build the custom object to be converted to JSON. Set APIKey as WindowsAuth if we are to use windows authentication.
-        $json = [pscustomobject] @{
-            "TimeoutSeconds" = 60
-            "Baseuri"  = $Baseuri
-            "Apikey"   = switch ($AuthType) {
-                WindowsIntegrated {
-                    ""
-                }
+        $json = [PSCustomObject]@{
+            TimeoutSeconds = 60
+            Baseuri = $Baseuri
+            Apikey = switch ($AuthType) {
+                WindowsIntegrated { "" }
                 WindowsCustom {
                     [pscustomobject]@{
                         "username" = $customcredentials.UserName
@@ -58,16 +56,24 @@
             }
             "AuthType" = $AuthType
         }
+        if ($SetPlainTextPasswords) {
+            if ($PSCmdlet.ShouldProcess('Allow passwords to be returned in plain text')) {
+                $json | Add-Member -NotePropertyName 'PasswordsInPlainText' -NotePropertyValue $SetPlainTextPasswords
+            } else { 
+                $json | Add-Member -NotePropertyName 'PasswordsInPlainText' -NotePropertyValue $false
+            }
+        } else {
+            $json | Add-Member -NotePropertyName 'PasswordsInPlainText' -NotePropertyValue $SetPlainTextPasswords
+        }
+
         if ($PasswordGeneratorAPIkey){
-            $json | Add-Member -MemberType NoteProperty -Name PasswordGeneratorAPIKey -Value ($PasswordGeneratorAPIkey | ConvertTo-SecureString -AsPlainText -Force | ConvertFrom-SecureString)
+            $json | Add-Member -NotePropertyName 'PasswordGeneratorAPIKey' -NotePropertyValue ($PasswordGeneratorAPIkey | ConvertTo-SecureString -AsPlainText -Force | ConvertFrom-SecureString)
         }
         $json = $json | ConvertTo-Json
     }
 
     end {
-        if ($PSCmdlet.ShouldProcess("$($profilepath)\passwordstate.json")) {
-            $json | Out-File "$($profilepath)\passwordstate.json"
-            $Script:Preferences.Path=$profilepath
-        }
+        $json | Out-File "$($profilepath)\passwordstate.json"
+        $Script:Preferences.Path=$profilepath
     }
 }
--- tests/functions/Set-PasswordStateEnvironment.Tests.ps1
+++ tests/functions/Set-PasswordStateEnvironment.Tests.ps1
@@ -12,6 +12,7 @@ InModuleScope 'PasswordState-Management' {
                 ,@{ParameterName='WindowsAuthOnly';Mandatory='False';ParameterSetName='Two'}
                 ,@{ParameterName='customcredentials';Mandatory='False';ParameterSetName='Three'}
                 ,@{ParameterName='Path';Mandatory='False';ParameterSetName='__AllParameterSets'}
+                ,@{ParameterName='SetPlainTextPasswords';Mandatory='False';ParameterSetName='__AllParameterSets'}
             )
             $AuthTestCases=@(
                 @{Baseuri='https://norealurl1.local'}
@@ -19,7 +20,8 @@ InModuleScope 'PasswordState-Management' {
                 @{Baseuri='https://norealurl3.local'}
             )
             $apiKey='somekey'
-            $Credential=[pscredential]::new('user',(ConvertTo-SecureString -AsPlainText -Force -String 'pass'))
+            $UserName = 'user'
+            $Credential=[pscredential]::new( $UserName ,(ConvertTo-SecureString -AsPlainText -Force -String 'pass'))
             $ProfilePath='TestDrive:\'
             $ProfileFile="$($ProfilePath)\passwordstate.json"
         }
@@ -74,6 +76,23 @@ InModuleScope 'PasswordState-Management' {
                 param($Baseuri)
                 Set-PasswordStateEnvironment -Baseuri $Baseuri -path $ProfilePath -WindowsAuthOnly
                 (Get-Content $ProfileFile | ConvertFrom-Json).AuthType | should -BeExactly "WindowsIntegrated"
+            }
+        }
+        Context 'Unit testing with Windows Custom Credential' {
+            It 'should verify if a json file is witten and Baseuri contains <baseuri>' -TestCases $AuthTestCases {
+                param($Baseuri)
+                Set-PasswordStateEnvironment -Baseuri $Baseuri -path $ProfilePath -customcredentials $Credential
+                (Get-Content $ProfileFile | ConvertFrom-Json).Baseuri | should -be $Baseuri
+            }
+            It 'Should verify if a json file is written and apikey has a credential' -TestCases $AuthTestCases {
+                param($Baseuri)
+                Set-PasswordStateEnvironment -Baseuri $Baseuri -path $ProfilePath -customcredentials $Credential
+                (Get-Content $ProfileFile | ConvertFrom-Json).apikey.username | should -BeExactly $UserName
+            }
+            It 'Should verify if a json file is written and AuthType is exactly "WindowsIntegrated" for <baseuri>' -TestCases $AuthTestCases {
+                param($Baseuri)
+                Set-PasswordStateEnvironment -Baseuri $Baseuri -path $ProfilePath -customcredentials $Credential
+                (Get-Content $ProfileFile | ConvertFrom-Json).AuthType | should -BeExactly "WindowsCustom"
             }
         }
     }
