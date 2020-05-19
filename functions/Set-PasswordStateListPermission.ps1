c85bedb08fa9bcd074f98b8b63019a062f07fdfb
eizedev <github@eize.dev>
2020-05-19 14:05:47 +0200

    Removed sort because it is currently not in use

--- functions/Set-PasswordStateListPermission.ps1
+++ functions/Set-PasswordStateListPermission.ps1
@@ -13,9 +13,7 @@
         [parameter(parameterSetName = 'PermissionID', Position = 3, ValueFromPipelineByPropertyName, Mandatory = $true)]
         [Nullable[System.Int32][]]$ApplyPermissionsForSecurityGroupID = $null,
         [parameter(parameterSetName = 'PermissionName', Position = 3, ValueFromPipelineByPropertyName, Mandatory = $true)]
-        [string]$ApplyPermissionsForSecurityGroupName = $null,
-        [parameter(ValueFromPipelineByPropertyName, Position = 4, Mandatory = $false)]
-        [switch]$Sort
+        [string]$ApplyPermissionsForSecurityGroupName = $null
     )
 
     begin {
@@ -117,7 +115,6 @@
             catch {
                 throw $_.Exception
             }
-
         }
     }
 
