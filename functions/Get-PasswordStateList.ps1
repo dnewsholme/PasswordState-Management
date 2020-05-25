268cac0eef5ea766cfb5373039885e8005f5b907
eizedev <github@eize.dev>
2020-05-25 15:03:06 +0200

    Added Reason parameter and fixed api uri if only passwordlistid was specified

--- functions/Get-PasswordStateList.ps1
+++ functions/Get-PasswordStateList.ps1
@@ -10,28 +10,37 @@
         [Parameter(ParameterSetName = 'Specific', ValueFromPipelineByPropertyName, Position = 2)][string]$TreePath,
         [Parameter(ParameterSetName = 'Specific', ValueFromPipelineByPropertyName, Position = 3)][int32]$SiteID,
         [Parameter(ParameterSetName = 'Specific', ValueFromPipelineByPropertyName, Position = 4)][string]$SiteLocation,
-        [parameter(ValueFromPipelineByPropertyName, Position = 5)][switch]$PreventAuditing
+        [parameter(ValueFromPipelineByPropertyName, Position = 5)][switch]$PreventAuditing,
+        [parameter(ValueFromPipelineByPropertyName, Position = 6)][string]$Reason
 
     )
 
+    Begin {
+        # Add a reason to the audit log if specified
+        If ($Reason) {
+            $headerreason = @{"Reason" = "$Reason" }
+            $parms = @{ExtraParams = @{"Header" = $headerreason } }
+        }
+        else { $parms = @{ } }
+    }
     Process {
         Switch ($PSCmdlet.ParameterSetName) {
             'ID' {
                 If (!($PSBoundParameters.ContainsKey('PasswordListID'))) {
-                    [string]$PasswordListID = ''
+                    $uri = "/api/passwordlists/"
                 }
                 else {
                     # if only PasswordListID is specified, so searching for a specific ID, PreventAuditing is not possible
-                    [switch]$PreventAuditing = $false
+                    $PreventAuditing = $false
+                    $uri = "/api/passwordlists/$($PasswordListID)"
                 }
-                $uri = "/api/passwordlists/$($PasswordListID)"
             }
             'Specific' {
                 $BuildURL = '?'
                 If ($PasswordList) { $BuildURL += "PasswordList=$([System.Web.HttpUtility]::UrlEncode($PasswordList))&" }
-                If ($Description) {  $BuildURL += "Description=$([System.Web.HttpUtility]::UrlEncode($Description))&" }
-                If ($TreePath) {     $BuildURL += "TreePath=$([System.Web.HttpUtility]::UrlEncode($TreePath))&" }
-                If ($SiteID) {       $BuildURL += "SiteID=$([System.Web.HttpUtility]::UrlEncode($SiteID))&" }
+                If ($Description) { $BuildURL += "Description=$([System.Web.HttpUtility]::UrlEncode($Description))&" }
+                If ($TreePath) { $BuildURL += "TreePath=$([System.Web.HttpUtility]::UrlEncode($TreePath))&" }
+                If ($SiteID) { $BuildURL += "SiteID=$([System.Web.HttpUtility]::UrlEncode($SiteID))&" }
                 If ($SiteLocation) { $BuildURL += "SiteLocation=$([System.Web.HttpUtility]::UrlEncode($SiteLocation))&" }
 
                 $BuildURL = $BuildURL -Replace ".$"
@@ -48,7 +57,7 @@
             }
         }
         Try {
-            Get-PasswordStateResource -uri $uri -method GET
+            Get-PasswordStateResource -uri $uri @parms -ErrorAction Stop
         }
         Catch {
             Throw $_.Exception
@@ -56,4 +65,4 @@
     }
 }
 
-Set-Alias -Name Find-PasswordstateList -Value Get-PasswordStateList -Force
+Set-Alias -Name Find-PasswordStateList -Value Get-PasswordStateList -Force
