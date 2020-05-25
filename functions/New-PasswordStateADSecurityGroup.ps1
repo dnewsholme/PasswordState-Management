d6d34ad865314006a4d784e6cace3714156cc422
eizedev <github@eize.dev>
2020-05-25 15:42:52 +0200

    Fixed Description parameter, added reason

--- functions/New-PasswordStateADSecurityGroup.ps1
+++ functions/New-PasswordStateADSecurityGroup.ps1
@@ -13,6 +13,14 @@
     )
 
     begin {
+        # Import PasswordState Environment for validation of PasswordsInPlainText setting
+        $PWSProfile = Get-PasswordStateEnvironment
+        # Add a reason to the audit log if specified
+        If ($Reason) {
+            $headerreason = @{"Reason" = "$Reason" }
+            $parms = @{ExtraParams = @{"Header" = $headerreason } }
+        }
+        else { $parms = @{ } }
     }
     process {
         # Build the Custom object to convert to json and send to the api.
@@ -23,18 +31,24 @@
         # Add description if parameter was given.
         # Currently the description of PasswordState is NOT synced during import of an AD Security group when using the API.
         # If this will be implemented in the future, the description is optional and will not be sent as an empty string or null in the request.
-        if ($Permission) {
+        if ($Description) {
             $body | Add-Member -NotePropertyName "Description" -NotePropertyValue $Description
         }
         # Adding API Key to the body if using APIKey as Authentication Type to use the api instead of winAPI
-        $penv = Get-PasswordStateEnvironment
-        if ($penv.AuthType -eq "APIKey") {
-            $body | Add-Member -MemberType NoteProperty -Name "APIKey" -Value $penv.Apikey
+        if ($PWSProfile.AuthType -eq "APIKey") {
+            $body | Add-Member -MemberType NoteProperty -Name "APIKey" -Value $PWSProfile.Apikey
         }
         if ($PSCmdlet.ShouldProcess("Creating AD Security Group '$SecurityGroupName' for Domain '$ADDomainNetBIOS' (Description: '$Description')")) {
             # Covert body to json and execute the api query
             $body = "$($body |ConvertTo-Json)"
-            $output = New-PasswordStateResource -uri "/api/securitygroup" -body $body -Sort:$Sort
+            if ($body) {
+                try {
+                    $output = New-PasswordStateResource -uri "/api/securitygroup" -body $body @parms -ErrorAction Stop
+                }
+                catch {
+                    throw $_.Exception
+                }
+            }
         }
     }
 
