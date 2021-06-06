0212f0072f2fc3256342542fd856a4ea9ef7c702
Trir262 <trammeke@hotmail.com>
2021-06-06 23:25:43 +0200

    Update pester build script

--- build/pester.ps1
+++ build/pester.ps1
@@ -1,9 +1,3 @@
-﻿push-Location $PSScriptRoot\..\functions
-$results = Invoke-Pester -EnableExit -PassThru -Show all -OutputFile $psscriptroot\report.xml -OutputFormat NUnitXml -CodeCoverage .\*.ps1
-& "$PSScriptRoot\nunit\msxsl.exe" "$PSScriptRoot\report.xml" "$PSScriptRoot\nunit\nunit-to-junit.xsl" -o "$PSScriptRoot\junitreport.xml"
-$failed = ($results.TestResult | Where-Object {$_.Passed -ne $true})
-if ($failed) {
-    $failed | Select-Object Context, Result, Name, Describe | Format-Table
-    throw "Some rules failed."
-}
+﻿push-Location $PSScriptRoot\..\tests\functions
+Invoke-Pester -Output Detailed
 Pop-Location
