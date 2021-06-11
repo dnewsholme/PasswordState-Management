4adbe7109474ef4c62b25da57ab1ee0ec6ca14cc
Trir262 <trammeke@hotmail.com>
2021-06-11 22:18:52 +0200

    Remove old new-pwsFolder tests

--- tests/functions/New-PasswordStateFolder.Tests.ps1
+++
@@ -1,7 +0,0 @@
-Remove-Module PasswordState-Management -Force -ErrorAction SilentlyContinue
-Import-module "$($PSScriptRoot)\..\..\Passwordstate-management.psd1" -Force
-. "$($PSScriptRoot)\json\enum-jsonFiles.ps1"
-InModuleScope -ModuleName 'PasswordState-Management' {
-    Describe "New-PasswordStateFolder" {
-    }
-}
