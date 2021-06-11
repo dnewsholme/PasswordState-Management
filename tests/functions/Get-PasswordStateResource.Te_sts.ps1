5ae608eba484de6628891ecce69f0e7912c75223
Trir262 <trammeke@hotmail.com>
2021-06-11 22:15:08 +0200

    prepare internal test but don't run it yet

--- tests/functions/Get-PasswordStateResource.Te_sts.ps1
+++ tests/functions/Get-PasswordStateResource.Te_sts.ps1
@@ -1,65 +1,82 @@
-﻿Remove-Module PasswordState-Management -Force -ErrorAction SilentlyContinue
-Import-module "$($PSScriptRoot)\..\..\Passwordstate-management.psd1" -Force
-. "$($PSScriptRoot)\json\enum-jsonFiles.ps1"
-InModuleScope 'PasswordState-Management' {
+﻿BeforeAll {
+    if (Test-Path "$([environment]::GetFolderPath('UserProfile'))\passwordstate.json") {
+        Rename-Item "$([environment]::GetFolderPath('UserProfile'))\passwordstate.json" "$([environment]::GetFolderPath('UserProfile'))\stowaway_passwordstate.json" -ErrorAction SilentlyContinue -Force -Confirm:$false
+    }
+    Import-Module -Name "$($PSScriptRoot)\..\..\passwordstate-management.psd1" -Force
+}
+AfterAll {
+    Remove-Module -Name 'passwordstate-management' -ErrorAction SilentlyContinue
+    if (Test-Path "$([environment]::GetFolderPath('UserProfile'))\stowaway_passwordstate.json") {
+        Rename-Item "$([environment]::GetFolderPath('UserProfile'))\stowaway_passwordstate.json" "$([environment]::GetFolderPath('UserProfile'))\passwordstate.json" -ErrorAction SilentlyContinue -Force -Confirm:$false
+    }
+}
+InModuleScope -ModuleName 'passwordstate-management' {
     Describe 'Get-PasswordStateResource' {
-        BeforeAll {
+            BeforeAll {
             $FunctionName='Get-PasswordStateResource'
-            $PreferencePath='TestDrive:'
+            $ProfilePath='TestDrive:'
             $BaseUri = 'https://passwordstate.local'
             $APIKey='SuperSecretStuff'
             $Credential=[pscredential]::new('MyUser',(ConvertTo-SecureString -AsPlainText -Force -String $APIKey))
-            $ParameterTestCases = @(
-                @{parameterName='Uri';Mandatory='False'}
-                @{parameterName='Method';Mandatory='False'}
-                @{parameterName='ContentType';Mandatory='False'}
-                @{parameterName='ExtraParams';Mandatory='False'}
-            )
-            Mock -CommandName 'Invoke-RestMethod' -MockWith {
-                $Global:TestJSON['PWSResponse']
-            }
+            $Paramattributetype='System.Management.Automation.ParameterAttribute'
+            . "$($PSScriptRoot)\json\enum-jsonFiles.ps1"
         }
-        Context 'Parameter validation' {
-            It 'Should have a parameter <ParameterName>' -TestCases $ParameterTestCases {
-                param($ParameterName)
-                (get-command $FunctionName).Parameters[$ParameterName] | Should -Not -BeNullOrEmpty
+        Context 'Parameter validation' -Foreach @(
+            @{parameterName='Uri';Mandatory='False'}
+            @{parameterName='Method';Mandatory='False'}
+            @{parameterName='ContentType';Mandatory='False'}
+            @{parameterName='ExtraParams';Mandatory='False'}
+        ) {
+            It 'Should have a parameter <parameterName>' {
+                (Get-Command -Name 'Get-PasswordStateResource').Parameters[$parameterName] | Should -Not -BeNullOrEmpty
             }
-            It 'Should have a parameter <parameter> with mandatory property set to <mandatory>' -TestCases $ParameterTestCases {
-                param($ParameterName, $Mandatory)
-                "$(((Get-Command -Name $FunctionName).Parameters[$ParameterName].Attributes | Where-Object { $_.gettype().Fullname -eq 'System.Management.Automation.ParameterAttribute'}).Mandatory)" | should -be $Mandatory
+            It 'Should have a parameter <parameter> with mandatory property set to <mandatory>' {
+                "$(((Get-Command -Name $FunctionName).Parameters[$parameterName].Attributes | Where-Object { $_.gettype().Fullname -eq $Paramattributetype}).Mandatory)" | should -be $Mandatory
             }
         }
         Context 'Unit Tests apikey' {
             BeforeAll {
-                Set-PasswordStateEnvironment -path $PreferencePath -Baseuri $BaseUri -Apikey $APIKey
+                Set-PasswordStateEnvironment -Uri $BaseURI -Apikey $APIKey -path $ProfilePath | Out-Null
+                Mock -CommandName 'Invoke-RestMethod' -ModuleName 'passwordstate-management' -MockWith { $Global:TestJSON['PWSResponse'] }
             }
             AfterAll {
-                Remove-Item 'TestDrive:\Passwordstate.json' -Confirm:$false -Force -ErrorAction SilentlyContinue
+                Remove-Item -Path "$([environment]::GetFolderPath("UserProfile"))\Passwordstate.json" -Force -Confirm:$false -ErrorAction SilentlyContinue
             }
             It 'Should return a response' {
-                Get-PasswordStateResource -uri '/api/response' | Should -Not -BeNullOrEmpty
+                InModuleScope -ModuleName 'passwordstate-management' {
+                    Get-PasswordStateResource -uri '/api/response' | Should -Not -BeNullOrEmpty
+                    Should -Invoke 'Invoke-RestMethod' -Exactly -Times 1 -Scope It
+                }
             }
         }
         Context 'Unit tests Windows authentication' {
             BeforeAll {
-                Set-PasswordStateEnvironment -path $PreferencePath -Baseuri $BaseUri -WindowsAuthOnly
+                Set-PasswordStateEnvironment -Uri $BaseURI -WindowsAuthOnly -path $ProfilePath | Out-Null
+                Mock -CommandName 'Invoke-RestMethod' -ModuleName 'passwordstate-management' -MockWith { $Global:TestJSON['PWSResponse'] }
             }
             AfterAll {
-                Remove-Item 'TestDrive:\Passwordstate.json' -Confirm:$false -Force -ErrorAction SilentlyContinue
+                Remove-Item -Path "$([environment]::GetFolderPath("UserProfile"))\Passwordstate.json" -Force -Confirm:$false -ErrorAction SilentlyContinue
             }
             It 'Should return a response' {
-                Get-PasswordStateResource -uri '/api/response' | Should -Not -BeNullOrEmpty
+                InModuleScope -ModuleName 'passwordstate-management' {
+                    Get-PasswordStateResource -uri '/api/response' | Should -Not -BeNullOrEmpty
+                    Should -Invoke 'Invoke-RestMethod' -Exactly -Times 1 -Scope It
+                }
             }
         }
         Context 'Unit tests Custom Credential' {
             BeforeAll {
-                Set-PasswordStateEnvironment -path $PreferencePath -Baseuri $BaseUri -customcredentials $Credential
+                Set-PasswordStateEnvironment -Uri $BaseURI -customcredentials $TestCredential -path $ProfilePath | Out-Null
+                Mock -CommandName 'Invoke-RestMethod' -ModuleName 'passwordstate-management' -MockWith { $Global:TestJSON['PWSResponse'] }
             }
             AfterAll {
-                Remove-Item 'TestDrive:\Passwordstate.json' -Confirm:$false -Force -ErrorAction SilentlyContinue
+                Remove-Item -Path "$([environment]::GetFolderPath("UserProfile"))\Passwordstate.json" -Force -Confirm:$false -ErrorAction SilentlyContinue
             }
             It 'Should return a response' {
-                Get-PasswordStateResource -uri '/api/response' | Should -Not -BeNullOrEmpty
+                InModuleScope -ModuleName 'passwordstate-management' {
+                    Get-PasswordStateResource -uri '/api/response' | Should -Not -BeNullOrEmpty
+                    Should -Invoke 'Invoke-RestMethod' -Exactly -Times 1 -Scope It
+                }
             }
         }
     }
